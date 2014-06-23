8d2f77f31b2f10fca0d4ed8ceb8e00327710bdee
Santiago Blankleider <santiago.blankleider@gmail.com>
2014-06-23 16:40:54 -0300

    agrego logo a mails, y quito enviar por mail el global por proveedor

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -86,9 +86,7 @@ class ProvidersController < ApplicationController
   def send_articles_provider
     begin
       provider = Provider.find(params[:id])
-
       UserMailer.new_articles_provider(provider).deliver
-      
       provider.get_articles_not_sent.each do |article|
         article.sent = true
         article.save
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,12 +6,14 @@ class UserMailer < ActionMailer::Base
     @provider = provider
     @list_articles_not_sent = @provider.get_articles_not_sent
     @url      = "localhost:3000/providers"
-	mail(to: @provider.email, subject: "Remito")
+    attachments.inline['logo.png'] = File.read('app/assets/images/logo.png')
+	  mail(to: @provider.email, subject: "Remito")
   end
 
   def send_billing_monthly(provider)
   	@provider = provider
   	@url      = "localhost:3000/providers"
+    attachments.inline['logo.png'] = File.read('app/assets/images/logo.png')
   	mail(to: @provider.email, subject: "Factura Mensual")
   end
 end
